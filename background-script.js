5edd258fbfdbc6dc6ab58fec46b37b1cf069374f
Danylo Berdnyk <danylob@ntnu.no>
2026-04-03 14:15:20 +0200

    initial setup that saves activity to local storage

---
+++ background-script.js
@@ -0,0 +1,82 @@
+const CHECK_ACTIVITY_PERIOD_IN_MINUTES = 1;
+
+let storageActivities;
+
+browser.alarms.onAlarm.addListener(handleAlarm);
+browser.alarms.create("checkActivity", {periodInMinutes: CHECK_ACTIVITY_PERIOD_IN_MINUTES});
+
+
+async function handleAlarm(alarmInfo) {
+    console.log(`on alarm: ${alarmInfo.name}; ${alarmInfo.periodInMinutes}`);
+    // console.log("updated");
+    if (alarmInfo.name === "checkActivity"){
+        console.log("we passed alarmInfo.name === \"checkActivity\"");
+    
+        const detectionIntervalInSeconds = 60*CHECK_ACTIVITY_PERIOD_IN_MINUTES;
+        let querying = browser.idle.queryState(detectionIntervalInSeconds);
+        querying.then(onGot);
+    }
+}
+
+function onGot(newState) {
+    if (newState === "idle") {
+        console.log("Idle state");
+    } else if (newState === "active") {
+        // we should experiment with it and query later, whats different between active and not hidden tab
+        browser.tabs.query({currentWindow: true, active: true}).then(logTabs, console.error);
+        console.log("Active state");
+    }
+}
+
+
+async function logTabs(tabs) {
+    if (tabs.length === 0){
+        return;
+    }
+    let tab = tabs[0]; // assume that there is only 1 active tab 
+    // getting url and hostname on the tab
+    const tabURL = tab.url;
+    const tabURLObj = new URL(tabURL);
+    const tabHostName = tabURLObj.hostname;
+
+    if (tabHostName === ""){
+        console.log("hostname is empty...returning");
+        return;
+    }
+    
+    console.log("you are on : ");
+    console.log(tabHostName);
+
+
+    storageActivities = await browser.storage.local.get("activities");
+    console.log(storageActivities);
+
+    const activityStorageIsEmpty = Object.keys(storageActivities).length === 0 && storageActivities.constructor === Object;
+    
+    if (activityStorageIsEmpty){
+        console.log("storage is empty!");
+        intialiseEmptyActivityStorage();
+    }
+
+    updateActivityTime(tabHostName, CHECK_ACTIVITY_PERIOD_IN_MINUTES);
+}
+
+const intialiseEmptyActivityStorage = async () => {
+    const newActivityStorageObj = {};
+    newActivityStorageObj["activities"] = {};
+    await browser.storage.local.set(newActivityStorageObj);
+}
+
+const updateActivityTime = async (hostName, timeToAdd) => {
+    if (storageActivities["activities"].hasOwnProperty(hostName)){
+        storageActivities["activities"][hostName] += timeToAdd;
+    } else {
+        storageActivities["activities"][hostName] = timeToAdd;
+    }
+    await browser.storage.local.set(storageActivities);
+}
+
+
+
+
+
